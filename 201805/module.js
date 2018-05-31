1ed03102207904c4445740e269e849c1e819d362
赵彬宇 <zhaobinyu@qijiayoudao.com>
2018-05-31 21:04:12 +0800

    update 20180531 完善编译部分

--- 201805/module.js
+++ 201805/module.js
@@ -180,6 +180,103 @@ Module.prototype.require = function(path) {
     return Module._load(path, this);
 };
 
+// Resolved path to process.argv[1] will be lazily placed here
+// (needed for setting breakpoint when called with --debug-brk)
+var resolvedArgv;
+
+
+/**
+ * 将文件在沙箱里运行 将暴露的变量提取出来
+ * @param content 文件字节流
+ * @param filename 文件路径
+ * @returns {*}
+ * @private
+ */
+Module.prototype._compile = function(content, filename) {
+    var self = this;
+    // remove shebang
+    content = content.replace(/^\#\!.*/, '');
+
+    function require(path) {
+        return self.require(path);
+    }
+
+    require.resolve = function(request) {
+        return Module._resolveFilename(request, self);
+    };
+
+    Object.defineProperty(require, 'paths', { get: function() {
+        throw new Error('require.paths is removed. Use ' +
+            'node_modules folders, or the NODE_PATH ' +
+            'environment variable instead.');
+    }});
+
+    require.main = process.mainModule;
+
+    // Enable support to add extra extension types
+    require.extensions = Module._extensions;
+    require.registerExtension = function() {
+        throw new Error('require.registerExtension() removed. Use ' +
+            'require.extensions instead.');
+    };
+
+    require.cache = Module._cache;
+
+    var dirname = path.dirname(filename);
+
+    if (Module._contextLoad) {
+        if (self.id !== '.') {
+            debug('load submodule');
+            // not root module
+            var sandbox = {};
+            for (var k in global) {
+                sandbox[k] = global[k];
+            }
+            sandbox.require = require;
+            sandbox.exports = self.exports;
+            sandbox.__filename = filename;
+            sandbox.__dirname = dirname;
+            sandbox.module = self;
+            sandbox.global = sandbox;
+            sandbox.root = root;
+
+            return runInNewContext(content, sandbox, { filename: filename });
+        }
+
+        debug('load root module');
+        // root module
+        global.require = require;
+        global.exports = self.exports;
+        global.__filename = filename;
+        global.__dirname = dirname;
+        global.module = self;
+
+        return runInThisContext(content, { filename: filename });
+    }
+
+    // create wrapper function
+    var wrapper = Module.wrap(content);
+
+    var compiledWrapper = runInThisContext(wrapper, { filename: filename });
+    if (global.v8debug) {
+        if (!resolvedArgv) {
+            // we enter the repl if we're not given a filename argument.
+            if (process.argv[1]) {
+                resolvedArgv = Module._resolveFilename(process.argv[1], null);
+            } else {
+                resolvedArgv = 'repl';
+            }
+        }
+
+        // Set breakpoint on module start
+        if (filename === resolvedArgv) {
+            global.v8debug.Debug.setBreakPoint(compiledWrapper, 0, 0);
+        }
+    }
+    var args = [self.exports, require, self, filename, dirname];
+    return compiledWrapper.apply(self.exports, args);
+};
+
 /**
  * 私有的加载文件
  * @param request 需要的文件名字或路径
